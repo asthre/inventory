a08d38f0b3017b299b8e6f80f41f731f91876a9b
asthre <166717235+asthre@users.noreply.github.com>
2025-02-20 22:54:58 +0800

    Feat: Creating item add form.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController; 
+
 
 Route::get('/login', function () {
     return view('login');
 });
 
+
 Route::get('/inventory', function () {
     return view('inventory');
 });
+
+
+Route::get('/items/create', [ItemController::class, 'create'])->name('items.create'); // Show form
+Route::post('/items', [ItemController::class, 'store'])->name('items.store'); // Handle form submission
